c9daa4a33786ad3fc8c0eadf88d87e897223b2ad
spireneko <spireneko@gmail.com>
2024-12-18 22:28:14 +0400

    feat: add form implementation

--- lab4/experience.js
+++ lab4/experience.js
@@ -1,26 +1,68 @@
 const form = document.getElementById('useless-form');
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
-const nameError = document.getElementById('nameError');
-const emailError = document.getElementById('emailError');
+const nameError = document.getElementById('name-error');
+const emailError = document.getElementById('email-error');
+const statusMessage = document.getElementById('status-message');
+
+
+let goodName = false;
+let goodEmail = false;
+
 
 nameInput.addEventListener('input', () => {
-    if (nameInput.value.length < 3) {
+    const name = nameInput.value.trim();
+
+    if (name.length < 3) {
         nameError.textContent = 'Имя должно содержать не менее 3 символов';
+        goodName = false
     } else {
         nameError.textContent = '';
+        goodName = true
     }
 });
 
-form.addEventListener('submit', (event) => {
-    if (nameInput.value.length < 3) {
-        event.preventDefault();
-        alert('Ошибка: имя слишком короткое');
-    }
 
+emailInput.addEventListener('input', () => {
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailPattern.test(emailInput.value)) {
-        event.preventDefault();
-        alert('Ошибка: некорректный email');
+        emailError.textContent = 'Некорректный email';
+        goodEmail = false
+    } else {
+        emailError.textContent = '';
+        goodEmail = true
+    }
+});
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const name = form.name.value.trim();
+    const email = form.email.value.trim();
+
+    if (!(goodName && goodEmail)) {
+        return;
+    }
+
+    const formData = new FormData(form);
+
+    try {
+        const response = await fetch('http://localhost:8000', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (response.ok) {
+            statusMessage.textContent = 'Форма успешно отправлена!';
+            statusMessage.style.color = 'green';
+
+            form.reset();
+        } else {
+            statusMessage.textContent = 'Ошибка: сервер вернул ошибку.';
+            statusMessage.style.color = 'red';
+        }
+    } catch (error) {
+        statusMessage.textContent = 'Ошибка: не удалось отправить форму.';
+        statusMessage.style.color = 'red';
     }
 });
